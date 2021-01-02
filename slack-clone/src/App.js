86cefb70cc5b550e3457cfb71e2e05aacebf0e2a
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-02 15:19:15 +0300

    be inhale and start

---
+++ slack-clone/src/App.js
@@ -0,0 +1,11 @@
+import './css/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello guys, ı am starting Slack clone with REACT.js</h1>
+    </div>
+  );
+}
+
+export default App;
