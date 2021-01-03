7136bb76df282d6da7a7634fa4b6a3d6d29b5886
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-03 20:13:15 +0300

    router-1

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,18 +1,30 @@
 import Header from './component/Header';
 import Sidebar from './component/Sidebar';
 import './css/App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     //BEM naming convention
     <div className='app'>
-      {/* Header*/}
-      <Header />
-      <div className='app__body'>
-        {/* Sidebar*/}
-        <Sidebar />
-        {/* React-Router->Chat screen*/}
-      </div>
+      <Router>
+        {/* Header*/}
+        <Header />
+        <div className='app__body'>
+          {/* Sidebar*/}
+          <Sidebar />
+          {/* React-Router->Chat screen*/}
+          <Switch>
+            <Route path='/room/:roomId'>
+              <h1>Chat screen</h1>
+            </Route>
+            <Route path='/'>
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
