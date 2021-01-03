e47160db82e365f6ee57fb841e0d704a5a68ca17
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-03 19:56:30 +0300

    sidebar finish

--- slack-clone/src/component/SidebarOption.js
+++ slack-clone/src/component/SidebarOption.js
@@ -9,7 +9,7 @@ function SidebarOption({ Icon, title }) {
         <h3>{title}</h3>
       ) : (
         <h3 className='sidebarOption__channel'>
-          <span className='sidebarOption__hash'>#</span>
+          <span className='sidebarOption__hash'>#{title}</span>
         </h3>
       )}
     </div>
