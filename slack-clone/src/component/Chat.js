c2c7884a7ddb392f1a79cdeb508e39466899afd5
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-04 19:08:22 +0300

    roomDetails

--- slack-clone/src/component/Chat.js
+++ slack-clone/src/component/Chat.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/Chat.css';
 import { useParams } from 'react-router-dom';
 import StarOutlinedIcon from '@material-ui/icons/StarOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import db from '../firebase';
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection('rooms')
+        .doc(roomId)
+        .onSnapshot(snapshot => setRoomDetails(snapshot.data()));
+    }
+  }, [roomId]);
+  console.log(roomDetails);
   return (
     <div className='chat'>
       <div className='chat__header'>
         <div className='chat__left'>
           <h4 className='chat__channelName'>
-            <strong>#general</strong>
+            <strong>#{roomDetails?.name}</strong>
             <StarOutlinedIcon />
           </h4>
         </div>
