cd0e033a4e76c1b14027f2e6e84cc56812b6df7a
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-04 23:47:45 +0300

    signIn-1

--- slack-clone/src/component/Login.js
+++ slack-clone/src/component/Login.js
@@ -1,8 +1,19 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import '../css/Login.css';
+import { auth, provider } from '../firebase';
 
 function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  };
   return (
     <div className='login'>
       <div className='login__container'>
@@ -12,7 +23,7 @@ function Login() {
         />
         <h1>Sign in to Ilayda Ozdemir Programmer HQ </h1>
         <p>ilaydaozdemir.slack.com</p>
-        <Button>Sıgn In with Google</Button>
+        <Button onClick={signIn}>Sıgn In with Google</Button>
       </div>
     </div>
   );
