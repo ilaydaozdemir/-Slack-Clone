9356cff2609af60e05f7c2de39fede1cddaf9707
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-04 23:03:15 +0300

    Login page-1

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,30 +1,37 @@
 import Header from './component/Header';
 import Sidebar from './component/Sidebar';
 import './css/App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Chat from './component/Chat';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     //BEM naming convention
     <div className='app'>
       <Router>
-        {/* Header*/}
-        <Header />
-        <div className='app__body'>
-          {/* Sidebar*/}
-          <Sidebar />
-          {/* React-Router->Chat screen*/}
-          <Switch>
-            <Route path='/room/:roomId'>
-              <Chat />
-            </Route>
-            <Route path='/'>
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <h1>LOGIN page</h1>
+        ) : (
+          <>
+            {/* Header*/}
+            <Header />
+            <div className='app__body'>
+              {/* Sidebar*/}
+              <Sidebar />
+              {/* React-Router->Chat screen*/}
+              <Switch>
+                <Route path='/room/:roomId'>
+                  <Chat />
+                </Route>
+                <Route path='/'>
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
