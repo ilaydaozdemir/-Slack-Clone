5523fdc3b7f642557016fae59fc7bcdc2b2444d5
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-04 18:39:47 +0300

    chat css

--- slack-clone/src/component/Chat.js
+++ slack-clone/src/component/Chat.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import '../css/Chat.css';
 import { useParams } from 'react-router-dom';
+import StarOutlinedIcon from '@material-ui/icons/StarOutlined';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 function Chat() {
   const { roomId } = useParams();
   return (
     <div className='chat'>
-      <h2>you {roomId}</h2>
+      <div className='chat__header'>
+        <div className='chat__left'>
+          <h4 className='chat__channelName'>
+            <strong>#general</strong>
+            <StarOutlinedIcon />
+          </h4>
+        </div>
+        <div className='chat__right'>
+          <p>
+            <InfoOutlinedIcon />
+            Details
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
